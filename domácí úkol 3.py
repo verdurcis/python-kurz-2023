025b67c32dd31da68e31b298d0a56ff05f42e490
verdurcis <129660617+verdurcis@users.noreply.github.com>
2023-04-18 22:56:39 +0200

    Add files via upload

---
+++ domácí úkol 3.py
@@ -0,0 +1,19 @@
+import json
+
+with open("zaci.json", encoding='utf-8') as soubor:
+     prospech = json.load(soubor)
+print(prospech)
+
+prospel = "Pass"
+neprospel = "Fail"
+
+for key,val in prospech.items():
+    if val >= 60:
+        prospech[key] = prospel
+    else:
+        prospech[key] = neprospel
+    print(prospech)
+
+    with open("prospech_json.json", mode="w", encoding='utf-8') as i:
+        json.dump(prospech, i, ensure_ascii=False)
+
